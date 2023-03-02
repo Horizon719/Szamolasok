4f5effc75bd49d5b7f1fdf98b2d7b799bfddfe41
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-02 13:35:52 +0100

    concoleról bekérés hogy ismétléssel írjuk e ki

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -1,13 +1,21 @@
 package ciklusok;
 
+import java.util.Scanner;
+
 public class Szamok {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ismetlessel vagy a nelkul irjuk ki? (i/n)");
+        String be = sc.nextLine();
         for (int k = 2; k < 5; k++) {
-            for (int i = 2; i < 4; i++) {
-                for (int j = 2; j < 5; j++) {
-                    System.out.printf("%d%d%d\n", k,i,j);
+                for (int i = 2; i < 5; i++) {
+                    for (int j = 2; j < 5; j++) {
+                        if(be.charAt(0) != 'i' && (i==j || i==k || j==k)){
+                            continue;
+                        }
+                        System.out.printf("%d%d%d\n", k,i,j);
+                    }
                 }
             }
-        }
     }
 }
