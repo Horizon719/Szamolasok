b6c75399e06bd341f6ebe31d784a0112783e80a5
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-02 13:44:09 +0100

    permutációk számának kiírása

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -7,15 +7,18 @@ public class Szamok {
         Scanner sc = new Scanner(System.in);
         System.out.print("Ismetlessel vagy a nelkul irjuk ki? (i/n)");
         String be = sc.nextLine();
+        int db = 0;
         for (int k = 2; k < 5; k++) {
-                for (int i = 2; i < 5; i++) {
-                    for (int j = 2; j < 5; j++) {
-                        if(be.charAt(0) != 'i' && (i==j || i==k || j==k)){
-                            continue;
-                        }
-                        System.out.printf("%d%d%d\n", k,i,j);
+            for (int i = 2; i < 5; i++) {
+                for (int j = 2; j < 5; j++) {
+                    if(be.charAt(0) != 'i' && (i==j || i==k || j==k)){
+                        continue;
                     }
+                    db++;
+                    System.out.printf("%d%d%d\n", k,i,j);
                 }
             }
+        }
+        System.out.printf("Permutaciok szama: %d\n", db);
     }
 }
