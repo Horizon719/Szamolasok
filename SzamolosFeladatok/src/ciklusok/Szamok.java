fd9f60c8631390d15d16f634fc86321248b5f06f
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-02 13:16:45 +0100

    3 egymásba ágyazott ciklus szamok ki

--- SzamolosFeladatok/src/ciklusok/Szamok.java
+++ SzamolosFeladatok/src/ciklusok/Szamok.java
@@ -2,6 +2,12 @@ package ciklusok;
 
 public class Szamok {
     public static void main(String[] args) {
-        
+        for (int k = 2; k < 5; k++) {
+            for (int i = 2; i < 4; i++) {
+                for (int j = 2; j < 5; j++) {
+                    System.out.printf("%d%d%d\n", k,i,j);
+                }
+            }
+        }
     }
 }
