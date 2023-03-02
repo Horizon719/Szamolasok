ca7b8f2ca2c058da2b3f3ab3b47d1a9a6e100863
Horizon719 <milichovszky.sandor@gmail.com>
2023-03-02 14:31:10 +0100

    sarkanyos jó szaám keresés javítása

--- SzamolosFeladatok/src/ciklusok/Sarkanyos.java
+++ SzamolosFeladatok/src/ciklusok/Sarkanyos.java
@@ -1,21 +1,18 @@
 package ciklusok;
 
-import java.util.Random;
-
 public class Sarkanyos {
     public static void main(String[] args) {
-        Random rnd = new Random();
         int x = 0;
         int y = 0;
-        boolean mehet = true;
-        while (mehet){
-            x = rnd.nextInt(20-1+1)+1;
-            y = rnd.nextInt(13-1+1)+1;
-            System.out.printf("7 * %d + 11 * %d = 145\n", x,y);
-            if(x< 21 && y<14 && (7*x + 11*y ==145)){
-                mehet = false;
+        while (x < 21){
+            x++;
+            y = 0;
+            while(y < 14){
+                y++;
+                if(7*x + 11*y == 145){
+                    System.out.printf("7 * %d(x) + 11 * %d(y) = 145\n", x,y);
+                }
             }
         }
-        
     }
 }
